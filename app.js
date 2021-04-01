433da22af8d4dd83686267a423a73e1414f620a7
jacsebastien <jac.sebastien@gmail.com>
2021-04-01 17:17:47 +0200

    Using middlewares

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const http = require('http');
 const express = require('express');
 
 const app = express();
-const server = http.createServer(app);
 
-server.listen(3000);
+app.use((req, res, next) => {
+  console.log("In the middleware !");
+  next(); // the request continue to the next middleware
+});
+
+app.use((req, res, next) => {
+  console.log("In the next middleware !");
+  res.send('<h1>Hello from Express !</h1>')
+});
+
+app.listen(3000)
