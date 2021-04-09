6429f51a0a0a60abc38d666588a694cb81586979
jacsebastien <jac.sebastien@gmail.com>
2021-04-09 16:33:02 +0200

    Implementing EJS templating

--- app.js
+++ app.js
@@ -2,20 +2,21 @@ const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const handlebars = require('express-handlebars');
+// const handlebars = require('express-handlebars');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.engine('hbs', handlebars({
-  layoutsDir: 'views/layouts/',
-  defaultLayout: 'main',
-  extname: 'hbs'
-}));
-app.set('view engine', 'hbs');
+// app.engine('hbs', handlebars({
+//   layoutsDir: 'views/layouts/',
+//   defaultLayout: 'main',
+//   extname: 'hbs'
+// }));
+// app.set('view engine', 'hbs');
 // app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views'); // Default value
 
 // Allow to parse and retrieve body of requests
@@ -32,7 +33,8 @@ app.use(shopRoutes);
 // router.use is triggerd for every kind of request (get/post/...)
 app.use((req, res, next) => {
   res.status(404).render('404', {
-    pageTitle: 'Page Not Found'
+    pageTitle: 'Page Not Found',
+    path: ""
   });
 });
 
