4a491ff5140460d7452a29f5419e3a8394572463
jacsebastien <jac.sebastien@gmail.com>
2021-11-24 11:48:14 +0100

    Create order based on cart Output order Clean cart on order creation

--- routes/shop.js
+++ routes/shop.js
@@ -10,7 +10,7 @@ router.get('/products/:id', shopController.getProduct);
 router.get('/cart', shopController.getCart);
 router.post('/cart', shopController.postCart);
 router.post('/cart/delete/:id', shopController.postCartDeleteProduct);
-router.get('/checkout', shopController.getCheckout);
+router.post('/create-order', shopController.postOrder);
 router.get('/orders', shopController.getOrders);
 
 module.exports = router;
