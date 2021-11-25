a222d83bb73af980922077174db3c15b03d8b17c
jacsebastien <jac.sebastien@gmail.com>
2021-11-25 17:21:49 +0100

    Authenticating the wrong way: using cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,27 @@
 exports.getLogin = (req, res, next) => {
+  const isAuthenticated = getCookieValue(req, 'isAuthenticated') === 'true';
+
   res.render('auth/login', {
     pageTitle: 'Login',
     active: {
       login: true,
     },
+    isAuthenticated
   });
 };
+
+exports.postLogin = (req, res, next) => {
+  res.setHeader('Set-Cookie', 'isAuthenticated=true');
+  res.redirect('/');
+};
+
+function getCookieValue(req, key) {
+  return req
+    .get('Cookie')
+    .split(';')
+    .reduce((returnedValue, cookieItem) => {
+      const [itemKey, value] = cookieItem.trim().split('=');
+      if (itemKey === key) return value;
+      return returnedValue;
+    }, '');
+}
